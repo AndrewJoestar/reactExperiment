cdf53e1be1a51b591fade595ef306b486d7cf239
Andrew Ikhtisar Afiq <andrew2000018073@webmail.uad.ac.id>
2025-07-25 09:05:03 +0700

    Push

---
+++ Fundamental/fundamental-react/src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+export default function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a simple component that displays a greeting.</p>
+    </div>
+  );
+}
+
+// export default HelloWorld;
