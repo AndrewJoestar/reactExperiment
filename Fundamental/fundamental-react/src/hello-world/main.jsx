eb91044e1f2d01d49c07a55ad36f8f54a1bd60ec
Andrew Ikhtisar Afiq <andrew2000018073@webmail.uad.ac.id>
2025-08-19 10:48:42 +0700

    pureComponent

--- Fundamental/fundamental-react/src/hello-world/main.jsx
+++ Fundamental/fundamental-react/src/hello-world/main.jsx
@@ -4,6 +4,11 @@ import Container from "./Container";
 import Table from "./table/Table";
 import { StrictMode } from "react";
 import TodoList from "../todo-list/Todolist";
+import AllertButton from "../button/AlertButton";
+import MyButton from "../button/MyButton";
+import ToolBar from "../button/ToolBar";
+import SearchForm from "../form/SearchForm";
+import SayHelloForm from "../form/SayHelloForm";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -11,6 +16,16 @@ createRoot(document.getElementById("root")).render(
       <HelloWorld />
       <TodoList />
       <Table></Table>
+      <AllertButton text="Click Me" message="Hello, World!" />
+      <MyButton text="Smash" onSmash={() => alert("Button smashed!")} />
+      <ToolBar
+        onClick={(e) => {
+          e.stopPropagation();
+          alert("Toolbar button clicked, thankyou!");
+        }}
+      />
+      <SearchForm />
+      <SayHelloForm />
     </Container>
   </StrictMode>
 );
